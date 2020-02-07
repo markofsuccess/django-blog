38ad4b64bc1feb3587ac70a61467febd8c857929
Markus Turesson Sjöman <markussjoman1@gmail.com>
2020-02-07 11:54:12 +0000

    corrected allows host name for herokue app

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get(''),'127.0.0.1','localhost', 'https://new-blog-test-app-django.herokuapp.com/' ]
+ALLOWED_HOSTS = [os.environ.get(''),'127.0.0.1','localhost', 'new-blog-test-app-django.herokuapp.com' ]
 
 
 # Application definition
